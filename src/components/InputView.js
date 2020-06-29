106524da2541b45eeff43b8d8c8bc0548b3a45f1
soharaki <soharaki@nissho-ele.co.jp>
2020-06-29 21:41:38 +0900

    日本測位系変換処理を追加

--- src/components/InputView.js
+++ src/components/InputView.js
@@ -3,8 +3,28 @@ import UIkit from 'uikit'
 import Icons from 'uikit/dist/js/uikit-icons'
 import 'uikit/dist/css/uikit.css'
 import 'uikit/dist/css/uikit.min.css'
-import Parser from "fast-xml-parser";
+import proj4 from 'proj4'
 UIkit.use(Icons);
+
+// こちらの記事を参考にさせていただきました。この場をお借りして感謝申し上げます。
+// JavaScriptでPROJ4JSを用いて旧日本測地系（SRID=4301）から世界測地系(SRID=4326)へ測地系変換
+// https://qiita.com/takahi/items/85732f577820d8f76b3e
+proj4.defs([
+    ["EPSG:4301", //東京測地系/日本測地系 SRID=4301
+        "+proj=longlat +ellps=bessel +towgs84=-146.414,507.337,680.507,0,0,0,0 +no_defs"
+    ]
+]);
+
+/**
+* tokyo2world(日本測地系から世界測地系に変換)
+* @param {lon} num - 日本測地系の経度
+* @param {lat} num - 日本測地系の緯度
+* @return {array} 0：世界測地系の経度 １:世界測地系の緯度
+**/
+function tokyo2world(lon, lat) {
+    var ret = proj4("EPSG:4301", "EPSG:4326", [lon, lat]);
+    return ret;
+}
 
 class InputView extends Component {
     constructor(props) {
@@ -54,7 +74,9 @@ class InputView extends Component {
                     }).then(res => {
                         return res.json();
                     }).then(json => {
-                        this.props.addMark(json[0].geometry.coordinates[1], json[0].geometry.coordinates[0]);
+                        // 日本測地系=>世界測地系に変更
+                        var ans = tokyo2world(json[0].geometry.coordinates[0], json[0].geometry.coordinates[1]);
+                        this.props.addMark(ans[1], ans[0]);
                     });
                 } else {
                     this.setState({ addressName: "正しい郵便番号を設定してください" });
